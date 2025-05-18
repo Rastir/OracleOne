0b77f0ac2144766cb20ad53cf71230ef58c74adf
Rastir <aldojero@hotmail.com>
2025-05-17 23:45:52 -0500

    started a new course on springboot

---
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
@@ -0,0 +1,10 @@
+package com.aluracursos.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record DatosSerie(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") Integer totalDeTemporadas,
+        @JsonAlias("imdbRating") String evaluacion) {
+}
