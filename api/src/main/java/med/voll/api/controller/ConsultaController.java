e19efc8431f1713eef61bf51efb177641fced69d
Rastir <aldojero@hotmail.com>
2025-07-05 23:40:53 -0500

    continued documentation course

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,6 +2,8 @@ package med.voll.api.controller;
 
 import med.voll.api.domain.consulta.DatosDetalleConsulta;
 import med.voll.api.domain.consulta.DatosReservaConsulta;
+import med.voll.api.domain.consulta.ReservaDeConsultas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +17,14 @@ import java.beans.Transient;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private ReservaDeConsultas reserva;
+
     @PostMapping
     @Transactional
     public ResponseEntity reservar (@RequestBody DatosReservaConsulta datos){
-        System.out.println(datos);
+
+        reserva.reservar(datos);
         return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
     }
 
---
+++ api/src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -0,0 +1,31 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservaDeConsultas {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @Autowired
+    private PacienteRepository pacienteRepository;
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void reservar(DatosReservaConsulta datos){
+
+        if(!pacienteRepository.existsById(datos.idPaciente())){
+            throw new ValidacionException("No existe un paciente con el Id" + datos.idPaciente());
+        }
+
+        var medico = medicoRepository.findById(datos.idMedico()).get();
+        var paciente = pacienteRepository.findById(datos.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, datos.fecha());
+        consultaRepository.save(consulta);
+    }
+}
